7fafb55eae417acbabc87bddad8fb81e11d76df6
Adam <cheradam2011@yandex.ru>
2026-09-21 11:59:35 +0700

    1 commit

--- src/utils/reverseNumber.ts
+++
@@ -1,9 +0,0 @@
-export const reverseNumber = (num: string) => {
-    let resultArr = [];
-
-    for (const char of num) {
-        resultArr.unshift(char);
-    };
-
-    return resultArr.join().replaceAll(",", "");
-};
